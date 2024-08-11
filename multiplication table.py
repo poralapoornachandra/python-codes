100d7f327782c2128bf5d27488b5294969801a22
porala poorna chandra MRU AIML3B(SL) <151039018+poralapoornachandra@users.noreply.github.com>
2024-08-11 15:14:39 +0530

    Update multiplication table.py

---
+++ multiplication table.py
@@ -0,0 +1,7 @@
+n=int(input())
+def multiplication_table(num1):
+    print("multiplication table of :",n)
+    for i in range(1,11):
+        print(f"{n} x {i} = {n*i}")
+multiplication_table(n)
+    
